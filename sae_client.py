962b2291e5a624dcd57e5fe871f2ced38745784e
Clément PASQUET <E22A210W@etu.polytech.univ-nantes.prive>
2023-06-08 15:31:32 +0200

    ajout commande

--- sae_client.py
+++ sae_client.py
@@ -1,13 +1,31 @@
 
-HOTE = '192.168.1.22'
+HOTE = '192.168.1.122'
 PORT= 8000
 
+import os
+import random
 import sys
 import socket
+if os.name == 'nt':
+    import msvcrt
+else:
+    import tty
+    import termios
+    import sys
+
+    def getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
 def clientUDP() :
 	mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	msgClient = input("Message ? ")
+	msgClient = input("Quelle difficulté ? ")
 	if msgClient == "":
 		print("Sortie du programme")
 		mySocket.close()
@@ -19,7 +37,24 @@ def clientUDP() :
 		
 		print("Echo reçu: ", msgServeur.decode('utf-8'))
 		
-		msgClient= input("Message ? (sinon taper FIN) ")
+		if os.name == 'nt':
+			key = msvcrt.getch().decode('utf-8')
+		elif os.name != 'nt':
+			try : 
+				key = getch()
+			except :
+				print("")
+		if key == 'a' or key == 'c':
+			break
+		elif key == 'q':
+			msgClient =b"gauche"
+		elif key == 'd':
+			msgClient =b"droite"
+		elif key == 'z':
+			msgClient =b"avant"
+		elif key == 's':
+			msgClient =b"arriere"
+
 		
 		if msgClient.upper() == "FIN" or msgClient == "":
 			break

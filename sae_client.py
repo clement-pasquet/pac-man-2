a1dd294c6d7c358dd4b0646eefa7c8da66b5f470
Justine Bernier <ju.bernier5@gmail.com>
2023-06-09 03:09:27 +0200

    version with all in the host server

--- sae_client.py
+++ sae_client.py
@@ -1,34 +1,47 @@
-
 HOTE = '127.0.0.1'
-PORT= 8000
+PORT = 8000
 
 import sys
 import socket
 import os
+import keyboard
 
 
-def clientUDP() :
-	mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	msgClient = input("Message ? ")
-	if msgClient == "":
-		print("Sortie du programme")
-		mySocket.close()
-		sys.exit()
-	while 1:
-		mySocket.sendto(bytes(msgClient, 'utf-8'), (HOTE, PORT))
-		
-		msgServeur, adresseServeur = mySocket.recvfrom(1024)
-		
-		print(msgServeur.decode('utf-8'), "Map Pac-Man")
-		
-		msgClient= input("Message ? (sinon taper FIN) ")
-		
-		if msgClient.upper() == "FIN" or msgClient == "":
-			break
-	
-	print("Fin du programme")
-	mySocket.close()
-	sys.exit()
-	
+def clientUDP():
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    msgClient = input("Message ? ")
+    mySocket.sendto(bytes(msgClient, 'utf-8'), (HOTE, PORT))
+    if msgClient == "":
+        print("Sortie du programme")
+        mySocket.close()
+        sys.exit()
+
+    while True:
+        if keyboard.is_pressed('z'):
+            print("Avancer")
+            msgClient = "avant"
+        elif keyboard.is_pressed('q'):
+            print("Gauche")
+            msgClient = "gauche"
+        elif keyboard.is_pressed('s'):
+            print("Arrière")
+            msgClient = "arriere"
+        elif keyboard.is_pressed('d'):
+            print("Droite")
+            msgClient = "droite"
+
+        mySocket.sendto(bytes(msgClient, 'utf-8'), (HOTE, PORT))
+
+        msgServeur, adresseServeur = mySocket.recvfrom(1024)
+
+        print(msgServeur.decode('utf-8'), "Map Pac-Man")
+
+        if msgClient.upper() == "FIN" or msgClient == "":
+            break
+
+    print("Fin du programme")
+    mySocket.close()
+    sys.exit()
+
 if __name__ == "__main__":
     clientUDP()
--- sae_python.py
+++ sae_python.py
@@ -7,57 +7,48 @@ import testPacman as pac
 import random
 
 def serveurUDP_simple():
-    
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
     mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
-    try:
-        mySocket.bind((HOTE,PORT))
-    except socket.error:
-        print("Liaison du socket UDP à l'adresse et au port choisit a échoué.")
-        sys.exit()
-        
-    print("Le serveur UDP écoute sur le port {}".format(PORT))
-    while 1:
-        msgClient, adresseClient = mySocket.recvfrom(1024)
 
-        pacman = pac.Pacman()
-        pacman.draw()
-        mapi = pacman.map
-        ghosts = [pac.Fantome() for _ in range(random.randint(1, 3))]
-        if msgClient.lower() == b"start":
-            print("msg serveur ici :",mapi)
-            msgServeur = bytes(pacman.map)
-            print("msg serveur ici :",msgServeur)
+    try:
+        mySocket.bind((HOTE, PORT))
+    except socket.error:
+        print("Liaison du socket UDP à l'adresse et au port choisis a échoué.")
+        sys.exit()
+
+    print("Le serveur UDP écoute sur le port {}".format(PORT))
+
+    pacman = pac.Pacman()
+    ghosts = [pac.Fantome() for _ in range(random.randint(1, 3))]
+
+    while True:
+        msgClient, adresseClient = mySocket.recvfrom(1024)
+        msgClient = msgClient.decode('utf-8')
+
+        if msgClient.lower() == "start":
+            # Initialiser la carte du Pac-Man
+            pacman = pac.Pacman()
+            current_map = pacman.draw()
+            msgServeur = bytes(current_map)
             mySocket.sendto(msgServeur, adresseClient)
-        #if msgClient.lower() == b"start":
-            
-        print("Message Client reçu: ", msgClient.decode('utf-8'))
-        msgServeur = msgClient
-        mySocket.sendto(msgServeur, adresseClient)
-        if msgClient.upper() == b"FIN" or msgClient == b"":
-            break
-    
-        msgServeur = msgClient
-        mySocket.sendto(msgServeur, adresseClient)
-        
-        #reglage difficulté
-        if msgClient.upper() == b"difficulte max":
-            pacman.difficulte = 10
-        #déplacements
-        if msgClient.upper() == b"droite":
-            pacman.move_right()
-        elif msgClient.upper() == b"gauche":
-            pacman.move_left()
-        elif msgClient.upper() == b"avant":
-            pacman.move_up()
-        elif msgClient.upper() == b"arriere":
-            pacman.move_down()
-        
+
+        elif msgClient.lower() in ["droite", "gauche", "avant", "arriere"]:
+            if msgClient.lower() == "droite":
+                pacman.move_right()
+            elif msgClient.lower() == "gauche":
+                pacman.move_left()
+            elif msgClient.lower() == "avant":
+                pacman.move_up()
+            elif msgClient.lower() == "arriere":
+                pacman.move_down()
+
+            current_map = pacman.draw()
+            msgServeur = bytes(current_map)
+            mySocket.sendto(msgServeur, adresseClient)
+
     print("Serveur fermé.")
     mySocket.close()
     sys.exit()
-    
+
 if __name__ == "__main__":
     serveurUDP_simple()
